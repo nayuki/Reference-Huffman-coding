1c6c1e11a10b410c00b973ad8a9e36f3de0cf2ab
Project Nayuki <me@nayuki.io>
2016-06-26 20:41:14 +0000

    Deleted trailing space.

--- java/src/HuffmanDecompress.java
+++ java/src/HuffmanDecompress.java
@@ -53,7 +53,7 @@ public final class HuffmanDecompress {
 		for (int i = 0; i < codeLengths.length; i++) {
 			// For this file format, we read 8 bits in big endian
 			int val = 0;
-			for (int j = 0; j < 8; j++) 
+			for (int j = 0; j < 8; j++)
 				val = (val << 1) | in.readNoEof();
 			codeLengths[i] = val;
 		}
